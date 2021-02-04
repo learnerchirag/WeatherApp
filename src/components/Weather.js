ceb8d314daab3c9301ccb787f649e8b93c31a167
Aviral Singh <avrlsngh@gmail.com>
2021-02-05 02:13:56 +0530

    UI Fixes

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -11,6 +11,8 @@ import Haze from "../mycollection/png/Haze.png";
 import Clear from "../mycollection/png/Clear.png";
 import pin from "../mycollection/png/pin.png";
 import { Container } from "react-bootstrap";
+import moment from 'moment';
+
 var d = new Date();
 export default class Weather extends Component {
   state = {
@@ -22,6 +24,7 @@ export default class Weather extends Component {
     today: null,
     days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
     dataLoaded: false,
+    sunTimingLoaded: false,
     chartCategories: [
       "7am",
       "8am",
@@ -54,6 +57,8 @@ export default class Weather extends Component {
       value: "chocolate",
       label: "Jaipur",
     },
+    sunTimingChartOptions: {},
+    sunTimingChartSeries: [],
   };
 
   componentDidMount() {
@@ -87,18 +92,16 @@ export default class Weather extends Component {
           },
           () => {
             this.updateChart(0);
+            this.updateSunTimingChart(0);
           }
         );
         console.log(data);
       });
   };
-  updateChart = async (index) => {
+  updateChart = (index) => {
     var chartData = [];
     let i;
     let n;
-    // this.state.hourly.forEach((hour) => {
-    //   chartData.push(hour.temp);
-    // });
     if (index % 2 === 0) {
       i = 0;
       n = 24;
@@ -106,13 +109,21 @@ export default class Weather extends Component {
       i = 24;
       n = 48;
     }
+    let xAxisDataSeries = [];
     for (i; i < n; i++) {
-      chartData.push(this.state.hourly[i].temp);
+      
+      const fetchedTemp = this.state.hourly[i].temp;
+      const fetchedDate = new Date(this.state.hourly[i].dt * 1000);
+      console.log('fetched Date: ', fetchedDate);
+      const timeHour = moment(fetchedDate).format('hh A');
+      const xAxisData = [Math.round(fetchedTemp).toString() + '°', timeHour];
+      xAxisDataSeries.push(xAxisData);
+      chartData.push(fetchedTemp);
     }
     console.log(chartData);
     var series = [
       {
-        name: "temperature",
+        name: "Temperature",
         data: chartData,
       },
     ];
@@ -120,7 +131,8 @@ export default class Weather extends Component {
     var options = {
       chart: {
         id: "area",
-        height: 2000,
+        height: 'auto',
+        // width: '100%',
         toolbar: {
           show: false,
         },
@@ -129,30 +141,24 @@ export default class Weather extends Component {
         enabled: false,
       },
       grid: {
-        show: false,
+        show: true,
+        yaxis: {
+          lines: {
+              show: false
+          }
+      }
       },
       stroke: {
-        show: true,
-        curve: "straight",
+        curve: "smooth",
       },
-      markers: {
-        size: 3,
-        shape: "circle",
-      },
+    
       yaxis: {
         show: false,
       },
       xaxis: {
-        categories: this.state.chartCategories,
+        categories: xAxisDataSeries,
       },
-      responsive: [
-        {
-          breakpoint: 540,
-          options: {
-            height: 400,
-          },
-        },
-      ],
+      
     };
     this.setState({
       series,
@@ -171,6 +177,66 @@ export default class Weather extends Component {
       return Clear;
     }
   };
+
+  updateSunTimingChart = (index) => {
+    console.log(this.state.daily[index], new Date(this.state.daily[index].sunrise));
+    const sunriseTime = moment(new Date(this.state.daily[index].sunrise * 1000)).format('hh A');
+    const sunsetTime = moment(new Date(this.state.daily[index].sunset * 1000)).format('hh A');
+    let sunTimingChartSeries = [
+      {
+        name: 'Time',
+        data: [0, 0.5, 0]
+      }
+    ]
+    let xAxisDataSeries = ['06 AM', '01 PM', '8 PM'];
+
+    let sunTimingChartOptions = {
+      chart: {
+        id: "area",
+        height: 100,
+        // width: '100%',
+        toolbar: {
+          show: false,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      grid: {
+        yaxis: {
+          lines: {
+            offsetX: -30
+          }
+        },
+        padding: {
+          left: 20
+        }
+      },
+      stroke: {
+        curve: "smooth",
+        lineCap: 'round',
+        colors: ['#e8ba56'],
+        width: 5
+      },
+    
+      yaxis: {
+        show: false,
+      },
+      xaxis: {
+        categories: xAxisDataSeries,
+      },
+      fill: {
+        colors: ['#e8ba56']
+      }
+      
+    }
+    this.setState({
+      sunTimingChartSeries,
+      sunTimingChartOptions,
+      sunTimingLoaded: true,
+    });
+    
+  }
   render() {
     return (
       <div>
@@ -179,27 +245,28 @@ export default class Weather extends Component {
             <Container fluid className="py-2">
               <div>
                 <Card
-                  className="shadow my-2 px-2"
-                  style={{ borderRadius: "15px" }}
+                  className="shadow-sm p-2"
+                  style={{ borderRadius: 15 }}
                 >
                   <div className="d-flex">
                     <div className="d-flex">
-                      <img className="my-auto" src={pin} />
+                      <img className="my-auto" src={pin} style={{height: 20}} />
                     </div>
-                    <div style={{ width: "100%" }}>
+                    <div style={{ width: "100%", outline: 'none' }}>
                       <Select
                         options={this.state.cityOptions}
                         placeholder="Select city"
+                        style={{outline: 'none'}}
                       />
                     </div>
                   </div>
                 </Card>
               </div>
               <Row
-                className="my-3"
+                className="my-3 daysTapsList"
                 style={{
                   flexWrap: "nowrap",
-                  overflow: "scroll",
+                  overflowX: "scroll",
                 }}
               >
                 {this.state.days.map((day, i) => (
@@ -213,100 +280,95 @@ export default class Weather extends Component {
                         {this.state.days[(this.state.today + i) % 7]}
                       </small>
                       <div className="d-flex">
-                        <small className="font-weight-bold mr-2">
-                          {Math.round(this.state.daily[i].temp.max)}&#8451;
+                        <small className="font-weight-bold mr-1">
+                          {Math.round(this.state.daily[i].temp.max)}°
                         </small>
                         <small>
-                          {Math.round(this.state.daily[i].temp.min)}&#8451;
+                          {Math.round(this.state.daily[i].temp.min)}°
                         </small>
                       </div>
                       <img
                         className="p-1"
+                        style={{height: 30}}
                         src={this.setIcon(this.state.daily[i].weather[0].main)}
                       />
-                      <small>{this.state.daily[i].weather[0].main}</small>
+                      <small className="text-muted">{this.state.daily[i].weather[0].main}</small>
                     </Card>
                   </Col>
                 ))}
               </Row>
 
               <Card
-                className="shadow border-0 py-2"
+                className="shadow-sm rounded border-0 p-4"
                 style={{ borderRadius: "12px" }}
               >
-                <Row>
+                <Row className="chartContainer">
                   <Col>
-                    <div className="d-flex">
-                      <h1 className="font-weight-bolder mr-3">
-                        {this.state.current.temp}&#8451;
+                    <div className="d-flex align-items-start">
+                      <h1 className="font-weight-bolder mr-3" style={{fontSize: 48}}>
+                        {this.state.current.temp}°C
                       </h1>
                       <img
+                      className="ml-1"
                         src={this.setIcon(this.state.current.weather[0].main)}
+                        height="30"
                       ></img>
                     </div>
                   </Col>
-                  <Col className="md-6 sm-12">
+                  <Col sm={12} className="overflow-scroll chartContainer w-100">
                     <div
                       // className="temp-chart"
-                      style={{ overflowX: "scroll", overflowY: "hidden" }}
+                      className="chartContainer w-100"
+                      style={{ overflowX: "scroll", overflowY: "hidden", zIndex: -10 }}
                     >
-                      <div style={{ minWidth: "1000px" }}>
+                      <div className="w-100 ml-2">
                         <Chart
                           // className="temp-chart"
                           options={this.state.options}
                           series={this.state.series}
-                          type="area"
+                          type="line"
+                          height="300"
+                          width="1100"
                         />
                       </div>
                     </div>
                   </Col>
                 </Row>
-                <Row>
+                <Row className="mt-3">
                   <Col>
                     <Card
-                      className="border-0"
+                      className="border-0 p-1 bg-light rounded shadow-sm"
                       style={{
-                        backgroundColor: "rgb(165, 217, 232)",
-                        textAlign: "center",
+                        textAlign: "left",
                       }}
                     >
-                      <small className="font-weight-bolder">Pressure</small>
-                      <small>{this.state.current.pressure}</small>
+                      <h6 style={{fontSize: 22}} className="font-weight-bolder">Pressure</h6>
+                      <p style={{fontSize: 20}} className="mb-0">{this.state.current.pressure} hpa</p>
                     </Card>
                   </Col>
                   <Col>
                     <Card
-                      className="border-0"
+                      className="border-0 p-1 bg-light rounded shadow-sm"
                       style={{
-                        backgroundColor: "rgb(165, 217, 232)",
-                        textAlign: "center",
+                        textAlign: "left",
                       }}
                     >
-                      <small className="font-weight-bolder">Humidity</small>
-                      <small>{this.state.current.humidity}</small>
+                      <h6 style={{fontSize: 22}} className="font-weight-bolder">Humidity</h6>
+                      <p style={{fontSize: 20}} className="mb-0">{this.state.current.humidity} %</p>
                     </Card>
                   </Col>
                 </Row>
                 <Row>
                   <Col>
-                    <div style={{ overflowX: "scroll", overflowY: "hidden" }}>
-                      <div style={{ Width: "100%" }}>
-                        {/* <Chart
-                          // className="temp-chart"
-                          options={this.state.options}
-                          series={[
-                            {name:"variation",
-                            data:[0,5,0]
-                          },
-                          {
-                            name:"sun",
-                            data:["1"]
-                          }
-                          ]}
-                          type="area"
-                        /> */}
-                      </div>
-                    </div>
+                  {this.state.sunTimingLoaded && (
+                    <Chart
+                    options={this.state.sunTimingChartOptions}
+                    series={this.state.sunTimingChartSeries}
+                    type="area"
+                    height="130"
+                     />
+                  )}
+                      
                   </Col>
                 </Row>
               </Card>
